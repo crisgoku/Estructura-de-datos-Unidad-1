ffa513c25af848e8f112a6c5b889939054df15b3
cristian <criscrafteano@hotmail.com>
2022-09-12 17:29:34 -0500

    segunda carga

---
+++ examen.py
@@ -0,0 +1,13 @@
+"""hacer un programa que pida un dato "cadena"y si dicha cadena comiza con un numero entoces se pasara a un arreglo
+#el programa no termina hasta que arreglo este lleno 5 datos   d = str (input)("escribe un dato ")"""
+
+
+for x in range(0,5):
+    d = (input)('escribe un dato:')
+    validarletras (d)    
+def validaletras(d):
+    if d == 0 and print('no es un numero '):
+    
+"""#haser un arreglo de 5 pociones que muestre una de forma ordenada las vocales
+
+#hacer un arreglo que lleve 20 pocioness exclusivamente con un digito de lo contario tiene que volorlo a pedir el progrma de mostrar la suma y promdio"""
---
+++ repaso.py
@@ -0,0 +1,29 @@
+"""# haser un progrma que en un arreglo de 10 posiones en las pociones pares se almacen numeros pares y en la de impares 
+#se amacen numeros impares 
+"""
+def arreglo():
+    n= int(input)('Escribe un numero difente de 0')
+    if(a[x]==0):
+        if ((x%2)==0 and (n%2)==0):
+            a[x]=n
+        if((x%2)!=0 and (n%2)!=0):
+            a[x]=n
+a =[0,0,0,0,0,0,0,0,0,0]
+for  x in range(0,10):
+   """ #arreglo()"""
+print(x,' ' )
+    """
+    #hacser un p
+    
+    
+def arreglo():
+    n= int(input)('Escribe un numero menor de 50')
+    if(a[x]==0):
+        if ((x%2)==0 and (n%2)==0):
+            a[x]=n
+        if((x%2)!=0 and (n%2)!=0):
+            a[x]=n
+a =[0,0,0,0,0,0,0,0,0,0]
+for  x in range(0,10):
+    #arreglo()
+    print(x,' ' )"""
